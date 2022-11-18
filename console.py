9f31de37f9908ff0cdca610fc1219270c197a041
KING$$ <tonyidragons@gmail.com>
2022-11-18 21:28:17 +0100

    🎁 github command

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ import psutil
 import logo
 import time
 import colors
+import webbrowser
 
 def write(phrase, speed):
     for char in phrase:
@@ -14,6 +15,7 @@ def write(phrase, speed):
 BYELLOW = '\033[93m'
 GREEN  = '\33[92m'
 RED    = '\33[31m'
+github = "https://github.com/Kingssxd/discordrpc"
 CBLUE   = '\33[94m'
 bs = 0.00000001
 
@@ -40,7 +42,6 @@ os.system("title DiscordRP")
 time.sleep(bs)
 os.system("title DiscordRPC")
 
-
 class commands(): 
     launch = "launch"
     configure = "configure"
@@ -48,14 +49,30 @@ class commands():
     invisiblelaunch = "invislaunch"
     stop = "stop"
     library = "libinstall"
+    github = "github"
+    update = "update"
+    settings = "settings"
 
+def githubi(): 
+    os.system("cls")
+    print(CBLUE + logo.logos.github)
+    write(BYELLOW + f"GitHub: {github}", bs)
+    write(BYELLOW + "\nPress 1 to open link, press enter to cotinue.", bs)
+    choice = input("\n > ")
+    if int(choice) == 1:
+        os.system(f"start \"\" {github}")
+        main()
+    else: 
+        time.sleep(0.1)
+        main()
+    
 def libinstall(): 
     print(" ")
     os.system("title DiscordRPC ^| Installing PSUTIL")
     os.system("pip install psutil")
     time.sleep(5)
     main()
-    
+
 def checkIfProcessRunning(processName):
     for proc in psutil.process_iter():
         try:
@@ -102,6 +119,7 @@ def helpfnc():
     write(CBLUE + "\n- invislaunch: launch the program with no cmd", bs)
     write(CBLUE + "\n- stop: stop the running invislaunch cmd", bs)
     write(CBLUE + "\n- libinstall: install required py libraries (psutil)", bs)
+    write(CBLUE + "\n- github: open github repository", bs)
     print(" ")
     input(BYELLOW + "\nPress enter to continue:D")
     main()
@@ -147,7 +165,9 @@ def main():
     if commandline == commands.stop: 
         stopinvis()
     if commandline == commands.library: 
-        libinstall()        
+        libinstall()     
+    if commandline == commands.github: 
+        githubi()   
     else:
         print(" ")
         write(RED + "This command doesn't exist!", bs)
